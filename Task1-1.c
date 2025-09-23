08f53daba2ef6c4d91c25e5c42aaf716fca666cc
anta1ke <20040007.zoxzox@gmail.com>
2025-09-23 11:13:34 +0300

    Create Task1-1.c

---
+++ Task1-1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+* @brief Расчитывает значение выражения А
+* @param X - значение параметра X
+* @param Y - значение параметра Y
+* @param Z - значение параметра Z
+* @return Возвращает значение
+*/
+double defA(const double X, const double Y, const double Z);
+
+/**
+* @brief Расчитывает значение выражения B
+* @param X - значение параметра X
+* @param Y - значение параметра Y
+* @param Z - значение параметра Z
+* @return Возвращает значение
+*/
+double defB(const double X, const double Y, const double Z);
+
+/**
+* @brief Точка входа в программу
+* @return Возращает 0, если программа была выполнена корректно, иначе 1
+*/
+int main(void)
+{
+	const double X = 2.0;
+	const double Y = 1.5;
+	const double Z = 0.8;
+
+	printf("A = %.6f\nB = %.6f", defA(X, Y, Z), defB(X, Y, Z));
+
+	return 0;
+}
+double defA(const double X, const double Y, const double Z)
+{
+	return(X * Y * Z + abs(Z * sin(Y)));
+}
+double defB(const double X, const double Y, const double Z)
+{
+	return(Y * cos(X * Z * sin(Y)) + 3);
+}
